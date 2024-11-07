0877eaf43c34b026e45dfb2726417ceef943bfa8
Chris Cooper <cjcooper252@gmail.com>
2024-11-07 12:34:45 -0800

    layout tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -36,31 +36,33 @@ function App() {
   const goToPlanSelection = () => setStep(2);
 
   return (
-    <div className="mx-auto flex min-h-screen max-w-5xl flex-col rounded-3xl md:grid md:grid-cols-[274px,1fr] md:bg-white md:p-4">
-      <div className="h-40 md:h-full">
-        <Header currentStep={currentStep} steps={STEPS} />
-      </div>
-      <div className="flex flex-col justify-between">
-        <main className="mx-4 -mt-14 max-w-xl rounded-lg bg-white p-6 shadow-lg md:mx-auto md:mt-2 md:w-full md:shadow-none">
-          {currentStep === 1 && (
-            <PersonalInfoForm formData={formValues} updateFields={handleUpdateFields} />
-          )}
-          {currentStep === 2 && (
-            <PlanSelectionForm formData={formValues} updateFields={handleUpdateFields} />
-          )}
-          {currentStep === 3 && (
-            <AddOnsForm formData={formValues} updateFields={handleUpdateFields} />
-          )}
-          {currentStep === 4 && <Summary formData={formValues} changePlan={goToPlanSelection} />}
-        </main>
-        <div className="fixed bottom-0 left-0 right-0 md:static">
-          <div className="mx-auto w-full max-w-[550px]">
-            <StepButtons
-              currentStep={currentStep}
-              totalSteps={STEPS.length}
-              onNext={handleNext}
-              onBack={handleBack}
-            />
+    <div className="flex min-h-screen md:items-center md:justify-center">
+      <div className="mx-auto flex h-full w-full max-w-5xl flex-col rounded-3xl md:grid md:min-h-[600px] md:grid-cols-[274px,1fr] md:bg-white md:p-4">
+        <div className="h-40 md:h-full">
+          <Header currentStep={currentStep} steps={STEPS} />
+        </div>
+        <div className="items flex flex-col justify-between">
+          <main className="mx-4 -mt-14 max-w-xl overflow-y-auto rounded-lg bg-white p-6 pb-24 shadow-lg md:mx-auto md:mt-2 md:w-full md:pb-6 md:shadow-none">
+            {currentStep === 1 && (
+              <PersonalInfoForm formData={formValues} updateFields={handleUpdateFields} />
+            )}
+            {currentStep === 2 && (
+              <PlanSelectionForm formData={formValues} updateFields={handleUpdateFields} />
+            )}
+            {currentStep === 3 && (
+              <AddOnsForm formData={formValues} updateFields={handleUpdateFields} />
+            )}
+            {currentStep === 4 && <Summary formData={formValues} changePlan={goToPlanSelection} />}
+          </main>
+          <div className="fixed bottom-0 left-0 right-0 md:static">
+            <div className="mx-auto w-full max-w-[550px]">
+              <StepButtons
+                currentStep={currentStep}
+                totalSteps={STEPS.length}
+                onNext={handleNext}
+                onBack={handleBack}
+              />
+            </div>
           </div>
         </div>
       </div>
